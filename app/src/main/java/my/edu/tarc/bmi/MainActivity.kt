99de5643ac93746dc7dbbabe3a47164fc343d995
xyan <62557207+bryanlsx@users.noreply.github.com>
2022-12-03 14:39:53 +0800

    Initial commit

---
+++ app/src/main/java/my/edu/tarc/bmi/MainActivity.kt
@@ -0,0 +1,66 @@
+package my.edu.tarc.bmi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+//    Module Level Variable = can be accessed by all module
+//    lateinit var = late initialize, give value later on
+//    val = value, var = variable
+    private lateinit var binding: ActivityMainBinding //associated with the layout
+//    recommend to use this view binder so no need to do multiple times
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+//        root = top of a tree structure -> layout
+        setContentView(binding.root) //load activity layout file to the memory
+        //Local Variable = any defined value within the scope
+
+        binding.buttonReset.setOnClickListener{
+
+        }
+
+        binding.buttonCalculate.setOnClickListener{
+            if(binding.editTextHeight.text.isEmpty()) {
+                binding.editTextHeight.setError(getString(R.string.value_required))
+            return@setOnClickListener
+            }
+
+            if(binding.editTextWeight.text.isEmpty()) {
+                binding.editTextWeight.setError(getString(R.string.value_required))
+                return@setOnClickListener
+            }
+
+            val weight = binding.editTextWeight.text.toString().toFloat()
+            val height = binding.editTextHeight.text.toString().toFloat()
+            val bmi = weight / (height/100).pow(2)
+
+            if(bmi < 18.5){
+                //Underweight
+                //BMI: 18.20 (underweight)
+                binding.textViewBMI.text = String.format("%s:%.2f (%s)", getString(R.string.app_name)
+                                                        , bmi, getString(R.string.underweight))
+
+                binding.imageViewBMI.setImageResources(R.drawable.under)
+            }else if(bmi > 25) {
+                //Overweight
+                binding.textViewBMI.text = String.format(
+                    "%s:%.2f (%s)",
+                    getString(R.string.app_name),
+                    bmi,
+                    getString(R.string.overweight)
+                )
+
+                binding.imageViewBMI.setImageResources(R.drawable.under)
+            }else {
+                //Normal
+                binding.textViewBMI.text = String.format("%s:%.2f (%s)", getString(R.string.app_name)
+                    , bmi, getString(R.string.underweight))
+
+                binding.imageViewBMI.setImageResources(R.drawable.normal)
+            }
+
+
+        }
+    }
+}
